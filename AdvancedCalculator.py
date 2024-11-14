d8adc81c8a277d067d8a42db30684fe3ef46a1b9
Vitor Henrique <117936669+VitorHSilver@users.noreply.github.com>
2024-11-14 10:20:49 -0300

    Update AdvancedCalculator.py

--- AdvancedCalculator.py
+++ AdvancedCalculator.py
@@ -10,20 +10,25 @@ while True:
     break
   if opcao == '1':
     print(f'\nOs valores {n1, n2}: resultado da soma {m.fsum([n1, n2]):.2f}')
-  if opcao == '2':
+  elif opcao == '2':
     print(f'\nOs valores {n1, n2}: resultado da subtração {n1-n2:.2f}')
-  if opcao == '3':
+  elif opcao == '3':
     print(f'\nOs valores {n1, n2}: resultado da multiplicação {n1*n2}')
-  if opcao == '4':
+  elif opcao == '4':
     print(f'\nOs valores {n1, n2}: resultado da divisão {n1 / n2} resultado inteiro {n1 // n2}')
-  if opcao == '5':
+  elif opcao == '5':
     print(f'\nOs valores arredondados para cima {n1, n2}: resultado {m.ceil(n1)} e {m.ceil(n2)}')
   elif opcao == '6':
     print(f'\nOs valores arredondados para baixo {n1, n2}: resultado {m.floor(n1)} e {m.floor(n2)}')
   elif opcao == '7':
     print(f'\nOs valores {n1, n2}: inteiros {m.trunc(n1)} e {m.trunc(n2)}')
   elif opcao == '8':
-    print(f'\nOs valores {n1, n2}: Raiz quadrada {m.sqrt(n1)} e {m.sqrt(n2)}')
+    if n1 and n2 < 0:
+      print('número invalido para essa operação, insira um novo número')
+    else:
+      n1_baixo= m.floor(n1)
+      n2_baixo= m.floor(n2)
+      print(f'\nOs valores {n1, n2}: Raiz quadrada {m.sqrt(n1_baixo)} e {m.sqrt(n2_baixo)}')
   elif opcao == '9':
     print(f'\nOs valores {n1, n2}: Potência {m.pow(n1,n2)} e {m.pow(n2,n1)}')
   elif opcao == '10':
@@ -31,8 +36,11 @@ while True:
     n2_int = int(n2)
     print(f'\nOs valores {n1, n2}: Raiz quadrada inteira {m.isqrt(n1_int)} e {m.isqrt(n2_int)}')
   elif opcao == '11':
-    n1_int = int(n1)
-    n2_int = int(n2)
-    print(f'\nOs valores {n1, n2}: Fatorial {m.factorial(n1_int)} e {m.factorial(n2_int)}')
+    if n1 and n2 < 0:
+      print('número invalido para essa operação, insira um novo número')
+    else:
+      n1_int = int(n1)
+      n2_int = int(n2)
+      print(f'\nOs valores {n1, n2}: Fatorial {m.factorial(n1_int)} e {m.factorial(n2_int)}')
   else:
     print('\n\nOpção invalida, tente novamente!')
